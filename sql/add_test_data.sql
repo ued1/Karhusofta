df02366abddc04ea5b43a182af9a1402a4a50f60
ued1 <ued1@users.noreply.github.com>
2015-09-12 13:24:17 +0300

    SQL ja dokumentaation päivitys

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,39 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Rooli (nimi, kuvaus, vaativuuskerroin) VALUES 
+    ('Autonkuljettaja', 'Kuljettaa pakoautoa', 5),
+    ('Murtoveikko', 'Murtautuu ja kerää saalisrahoja', 7),
+    ('Puolustaja', 'Puolustaa muita ja taistelee vihollisia vastaan', 6),
+    ('Tarkkailija', 'Tarkkailee ympäristöä ja varoittaa tarvittaessa muita', 2);
+
+INSERT INTO Karhu (nimi, salasana, saldo) VALUES 
+    ('testikarhu1', 'salasana1', 0),
+    ('testikarhu2', 'salasana2', 1),
+    ('testikarhu3', 'salasana3', 0),
+    ('testikarhu4', 'salasana4', 0),
+    ('testikarhu5', 'salasana5', 32700),
+    ('testikarhu6', 'salasana6', 0),
+    ('testikarhu7', 'salasana7', 0),
+    ('testikarhu8', 'salasana8', 0),
+    ('testikarhu9', 'salasana9', 0);
+
+INSERT INTO Osaaminen(karhuID, rooliID) VALUES
+    (1, 1),
+    (1, 4),
+    (2, 2),
+    (3, 2),
+    (4, 3),
+    (5, 3),
+    (6, 4),
+    (7, 1);
+
+INSERT INTO Kohde (nimi, osoite, kuvaus, arvo) VALUES 
+    ('Ankkalinnan pankki', 'Ankkalinnankatu 1', 'Pankki, jossa on paljon rahaa', 100000),
+    ('Roope Ankan kioski', 'Roopentie 1', NULL, 500);
+
+INSERT INTO Keikka (nimi, osallistujamaara, kohdeID, karhuID) VALUES
+    ('Ryöstetään Roopen kioski', 5, 2, 5);
+
+INSERT INTO Osallistuminen (keikkaID, karhuID, rooliID) VALUES
+    (1, 5, 3),
+    (1, 1, 1),
+    (1, 2, 2);
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,51 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Rooli(
+    rooliID SERIAL PRIMARY KEY,
+    nimi varchar(20) UNIQUE NOT NULL,
+    kuvaus varchar(120),
+    vaativuuskerroin INTEGER NOT NULL
+);
+
+CREATE TABLE Karhu(
+    karhuID SERIAL PRIMARY KEY,
+    nimi varchar(20) UNIQUE NOT NULL,
+    salasana varchar(20) NOT NULL,
+    saldo INTEGER NOT NULL
+);
+
+CREATE TABLE Osaaminen(
+    karhuID INTEGER REFERENCES Karhu(karhuID),
+    rooliID INTEGER REFERENCES Rooli(rooliID),
+    PRIMARY KEY (karhuID, rooliID)
+);
+
+CREATE TABLE Kohde(
+    kohdeID SERIAL PRIMARY KEY,
+    nimi varchar(20) NOT NULL,
+    osoite varchar(50),
+    kuvaus varchar(500),
+    arvo INTEGER NOT NULL
+);
+
+CREATE TABLE Keikka(
+    keikkaID SERIAL PRIMARY KEY,
+    nimi varchar(50) UNIQUE NOT NULL,
+    osallistujamaara INTEGER NOT NULL,
+    kohdeID INTEGER REFERENCES Kohde(kohdeID) NOT NULL,
+    karhuID INTEGER REFERENCES Karhu(karhuID) NOT NULL
+);
+
+CREATE TABLE Tulos(
+    tulosID SERIAL PRIMARY KEY,
+    keikkaID INTEGER REFERENCES Keikka(keikkaID) UNIQUE NOT NULL,
+    paiva DATE NOT NULL,
+    kuvaus varchar(120) NOT NULL,
+    saalis INTEGER NOT NULL
+);
+
+CREATE TABLE Osallistuminen(
+    keikkaID INTEGER REFERENCES Keikka(keikkaID),
+    karhuID INTEGER REFERENCES Karhu(karhuID),
+    rooliID INTEGER REFERENCES Rooli(rooliID),
+    PRIMARY KEY (keikkaID, karhuID)
+);
+
